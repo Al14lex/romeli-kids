0864d6687639659b6c12e60cb96126f4aab52c33
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-08-27 12:05:20 +0200

    add frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  server: { port: 5173, proxy: { '/api': 'http://localhost:3001' } },
+  build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        boys: resolve(__dirname, 'boys.html'),
+        girls: resolve(__dirname, 'girls.html'),
+        contact: resolve(__dirname, 'contact.html'),
+        admin: resolve(__dirname, 'admin.html'),
+      }
+    }
+  }
+})
