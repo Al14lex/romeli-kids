9a633dd4c5dba4a109a25918671d4d9364de295e
Aleksandra Khomenko <a.khomenko1421@gmail.com>
2025-09-28 21:59:10 +0200

    fbsb

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5000",
-    "http://localhost:3000",
+    "http://localhost:5173",
     "https://romeli-kids.vercel.app"
   ],
   methods: ["GET", "POST", "PATCH", "DELETE"],
